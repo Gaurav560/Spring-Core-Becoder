d97bca5cd29b8985494c4ed3285385de15a5ecd2
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-03 19:42:10 +0530

    ambuiguity problems in constructor injection and their solution through name,index and type parameters in constructor-arg tag

---
+++ Spring_core/src/main/java/com/ineuron/ai/Ci/Certificate.java
@@ -0,0 +1,16 @@
+package com.ineuron.ai.Ci;
+
+public class Certificate {
+
+	
+	private int id;
+	private String certName;
+	
+		
+	public Certificate(int id, String certName) {
+		super();
+		this.id = id;
+		this.certName = certName;
+	}
+	
+}
