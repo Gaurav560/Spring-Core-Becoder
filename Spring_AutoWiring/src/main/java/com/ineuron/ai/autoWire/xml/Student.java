cf0db9e2aef0b9b38ac563df003ac58b4b91bf83
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-08 01:19:19 +0530

    used lombok instead of getters and setters and to String method

--- Spring_AutoWiring/src/main/java/com/ineuron/ai/autoWire/xml/Student.java
+++ Spring_AutoWiring/src/main/java/com/ineuron/ai/autoWire/xml/Student.java
@@ -1,29 +1,14 @@
 package com.ineuron.ai.autoWire.xml;
 
+import lombok.Data;
+
+@Data
 public class Student {
 
-	
-	
 	private Address address;
 
-	
-	
-	public Address getAddress() {
-		return address;
-	}
 
 
-
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Student [address=" + address + "]";
-	}
 
 
 
