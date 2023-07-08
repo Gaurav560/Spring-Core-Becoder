8918e6d7ea494e1aa0bc44b5ae6eef276cb7225f
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-08 15:50:08 +0530

    use of primary attribute when we use xml based autowiring with the byType method,when one class has more than one bean .SPring get confuse ,that which bean should be inserted in target bean .so primary should be used in that bean with true or false .

--- Spring_AutoWiring/src/main/java/com/ineuron/ai/autoWire/xml/Student.java
+++ Spring_AutoWiring/src/main/java/com/ineuron/ai/autoWire/xml/Student.java
@@ -2,13 +2,33 @@ package com.ineuron.ai.autoWire.xml;
 
 import lombok.Data;
 
-@Data
+
 public class Student {
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	public Student(Address address) {
 		super();
 		this.address = address;
 	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [address=" + address + "]";
+	}
+
+	
 
 	private Address address;
 
