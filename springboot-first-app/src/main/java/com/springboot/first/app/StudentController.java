9cb8958f5369fa850a1fdb08979f3d09241a4153
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-19 17:44:35 +0530

    creating rest api to handle path variables

--- springboot-first-app/src/main/java/com/springboot/first/app/StudentController.java
+++ springboot-first-app/src/main/java/com/springboot/first/app/StudentController.java
@@ -1,6 +1,10 @@
 package com.springboot.first.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,27 @@ public class StudentController {
 	@GetMapping("/student/")
 	public Student getStudentObject() {
 		return new Student("gaurav","sharma");
+	}
+	
+	
+	@GetMapping("/get-Students/")
+	public List<Student> getStudentObjects(){
+		
+		List<Student> students=new ArrayList<>();
+		students.add(new Student("amar","sharma"));
+		students.add(new Student("sameer","sharma"));
+		students.add(new Student("piyush","sharma"));
+		students.add(new Student("puchu","sharma"));
+		students.add(new Student("divya","sharma"));
+		return students;
+	}
+	
+	
+	
+	//creating rest api to handle pathVariable
+
+	@GetMapping("/student/{firstName}/{lastName}")
+	public Student studentPathVariable(@PathVariable("firstName") String firstName1,@PathVariable("lastName") String lastName1) {
+		return new Student(	firstName1, lastName1);
 	}
 }
