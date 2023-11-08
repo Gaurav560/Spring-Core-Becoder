03b6086f8f23ebb636b4ee02b56998c1d3da215e
GAURAV SHARMA <gsharma1154@gmail.com>
2023-11-09 02:55:19 +0530

    first commit for enotes spring boot project .kaafi samajh mein aa raha hai .

---
+++ enotes_spring_boot_project/src/main/java/com/ineuron/ai/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.ineuron.ai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//controller request receive karta hai jo bhi browser se send hota hai 
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/add_notes")
+	public String add_notes() {
+		return "add_notes";
+		
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+	return "register";	
+	}
+	
+	@GetMapping("/view_notes")
+	public String view_notes() {
+		return "view_notes";
+	}
+	
+	@GetMapping("/edit_notes")
+	public String edit_notes() {
+		return "edit_notes";
+	}
+}
---
+++ enotes_spring_boot_project/src/main/java/com/ineuron/ai/controller/package-info.java
@@ -0,0 +1,1 @@
+package com.ineuron.ai.controller;
